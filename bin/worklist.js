1475f1529a81a7fd6a02a234f0c96e870577467f
Darel Finkbeiner <finkbeinerd@osti.gov>
2014-04-24 11:04:15 -0400

    add travis, switch binary

--- bin/worklist.js
+++ bin/worklist.js
@@ -1,48 +1,37 @@
 #!/usr/bin/env node
 
-var fs = require('fs')
+var WorkList = require('worklist')
 var moment = require('moment')
 
-var defaults = { weekly: true, workdir: process.env.HOME + '/worklist/' }
+var defaults = { weekly: true, dbname: process.env.HOME + '/worklist/worklist.db' }
 var argv = require('yargs').boolean('weekly').default(defaults).alias('weekly', 'w').argv
 var m = moment()
-if (argv.weekly) {
-  m = moment().startOf('week').add('days', 1)
-}
 
-var fname = argv.workdir + m.format('YYYY-MM-DD') + '.txt'
+var list = new WorkList(defaults)
 
-var contents = []
-fs.readFile(fname, function(err, data) {
-  if (!err) {
-    contents = data.toString().split('\n').filter(Boolean)
-  }
-  parseCommands()
-})
-
-function saveContents() {
-  fs.writeFile(fname, contents.join('\n') + '\n')
-}
 
 function parseCommands() {
   if (argv._.indexOf('list') > -1) {
-    contents.forEach(function(v,i) { console.log("%d: %s", i, v) })
+    list.getCurrentList(function(data) {
+      data.forEach(function(v,i) { console.log("%d: %s", i, v.value) })
+    })
   }
   if (argv._.indexOf('add') > -1) {
     var position = argv._.indexOf('add')+1
     if (argv._[position]) {
-      contents.push(argv._[position])
-      saveContents()
+      list.addEntry(argv._[position])
     }
   }
   if (argv._.indexOf('delete') > -1) {
     var position = argv._.indexOf('delete')+1
-    if (argv._[position]) {
+    if (argv._[position] > -1) {
       var i = argv._[position]
-      if (contents.length > i) {
-        delete contents[i]
-        saveContents()
-      }
+      list.getCurrentList(function() {
+        list.deleteEntry(i)
+      })
     }
   }
 }
+
+
+parseCommands()
